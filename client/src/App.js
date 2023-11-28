3d146e66f1fd30fd8bc91f8420d49d086c54576d
fahdaroua <fahd.aroua@gmail.com>
2023-11-28 22:37:18 +0100

    profile user

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./styles.css";
 import Log from "./component/signup";
+import ProfilePage from "./component/ProfilePage";
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path='/register' element={<Log />} />
+          <Route path='/' element={<Log />} />
+          <Route path='/Profile' element={<ProfilePage />} />
         </Routes>
       </Router>
     </>
